2d01b65610b16e4e04bb8c154eab62142a0e1ce8
andrzejo <andrzej.oczkowicz@gmail.com>
2016-01-07 17:04:04 +0100

    More logs

--- public/js/long-poll.js
+++ public/js/long-poll.js
@@ -17,6 +17,7 @@ function longPoll() {
     }).always(function (events, textStatus) {
         if (textStatus != 'abort') {
             $.each(events, function () {
+                logMessage("Event: " + this.name + ", params: " + JSON.stringify(this.params));
                 eventBus.trigger(this.name, this.params);
             });
             longPoll();
@@ -24,7 +25,7 @@ function longPoll() {
     }).fail(function (jqXHR, textStatus, errorThrown) {
         var response = jqXHR.responseText || '(null)';
         var msg = "long_pool failed! status: '" + textStatus + "', errorThrown: '" + errorThrown + "'. Response text: '" + response + "'.";
-        logError(msg);
+        logMessage(msg);
         //throw new Error(msg);
     });
 }
