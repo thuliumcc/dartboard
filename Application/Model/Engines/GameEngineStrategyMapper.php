97f04b023e08bc2b3475bd409b59ae41aca98e87
piotrooo <piotroo89@gmail.com>
2016-01-10 13:26:52 +0100

    change type generating game type names

--- Application/Model/Engines/GameEngineStrategyMapper.php
+++ Application/Model/Engines/GameEngineStrategyMapper.php
@@ -3,13 +3,19 @@ namespace Application\Model\Engines;
 
 use Application\Model\Game;
 use BadMethodCallException;
+use Exception;
 use Ouzo\Utilities\Arrays;
 
 class GameEngineStrategyMapper
 {
-    private static $map = [
+    private static $classMap = [
         'cricket' => '\Application\Model\Engines\Cricket',
         '101' => '\Application\Model\Engines\Game101',
+    ];
+
+    private static $nameMap = [
+        'cricket' => 'Cricket',
+        '101' => '101'
     ];
 
     /**
@@ -19,10 +25,25 @@ class GameEngineStrategyMapper
     public static function instance(Game $game)
     {
         $type = $game->type;
-        $class = Arrays::getValue(self::$map, $type);
+        $class = Arrays::getValue(self::$classMap, $type);
         if ($class) {
             return new $class($game);
         }
         throw new BadMethodCallException('Unknown type [' . $type . ']');
     }
+
+    public static function getTypeNames()
+    {
+        $typeNamesMap = [];
+        foreach (self::$classMap as $type => $class) {
+            $name = Arrays::getValue(self::$nameMap, $type);
+            if ($name) {
+                $typeNamesMap[$type] = $name;
+            } else {
+                throw new Exception('Not defined name for the game type [' . $type . ']');
+            }
+        }
+        return $typeNamesMap;
+    }
+
 }
